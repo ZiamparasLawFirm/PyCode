d1e81fc831bb23cb0899fa9f22780fa7c611b921
dimitrisziamparas <dimitris.ziamparas@yahoo.com>
2025-08-07 00:30:55 +0300

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -15,19 +15,41 @@ args = parser.parse_args()
 llm = OpenAI()
 
 code_prompt = PromptTemplate(
+    input_variables=["task", "language"],
     template="Write a very short {language} "
-             "function that will {task}",
-    input_variables=["language", "task"]
+             "function that will {task}"
+)
+
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test for the following {language} code:\n {code}"
 )
 
 code_chain = LLMChain(
     llm=llm,
     prompt=code_prompt,
+    output_key="code"
 )
 
-result = code_chain({
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["task", "language"],
+    output_variables=["test", "code"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-print(result["text"])
+print(">>>>>> GENERATED CODE")
+print(result["code"])
+
+print(">>>>>> GENERATED TEST")
+print(result["test"])
